23c93f1446bba4a52dbb6be4bcdae1802f3ae36c
VasAtanasov <vas.atanasov@gmail.com>
2025-06-18 00:36:00 +0300

    do - m2

---
+++ 202506-containerization-and-infrastructure/20250617-advanced-containerization-concepts/m2/a64/readme.txt
@@ -0,0 +1,3 @@
+Will be added upon request.
+
+It is essentially the same as the one for x64 (Intel64/AMD64).
---
+++ 202506-containerization-and-infrastructure/20250617-advanced-containerization-concepts/m2/x64/almalinux/docker-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "* Add hosts ..."
+echo "192.168.99.100 docker.do1.lab docker" >> /etc/hosts
+
+echo "* Add Docker repository ..."
+dnf -y install dnf-plugins-core
+dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+
+echo "* Install Docker ..."
+dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "* Enable and start Docker ..."
+systemctl enable docker
+systemctl start docker
+
+echo "* Firewall - open port 8080 ..."
+firewall-cmd --add-port=8080/tcp --permanent
+firewall-cmd --reload
+
+echo "* Add vagrant user to docker group ..."
+usermod -aG docker vagrant
