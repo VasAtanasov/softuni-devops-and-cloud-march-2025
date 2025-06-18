41b6cdc9ebe09b5b154fe187198bb8eb8eb26648
VasAtanasov <vas.atanasov@gmail.com>
2025-06-18 11:48:27 +0300

    do - m2: homework

---
+++ 202506-containerization-and-infrastructure/20250617-advanced-containerization-concepts/hm-m1/docker-tools.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+echo "# LazyDocker"
+LAZYDOCKER_VERSION=$(curl -sL "https://api.github.com/repos/jesseduffield/lazydocker/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
+curl -L -o lazydocker.tar.gz "https://github.com/jesseduffield/lazydocker/releases/download/v${LAZYDOCKER_VERSION}/lazydocker_${LAZYDOCKER_VERSION}_Linux_x86_64.tar.gz"
+tar xzvf lazydocker.tar.gz lazydocker
+install -Dm 755 lazydocker -t "/home/vagrant/.local/bin"
+rm lazydocker lazydocker.tar.gz
+
+echo "# Dive"
+DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
+curl -fOL "https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb"
+apt-get install -y ./dive_${DIVE_VERSION}_linux_amd64.deb
+rm ./dive_${DIVE_VERSION}_linux_amd64.deb
+
+echo "# Portrainer"
+docker volume create portainer_data
+docker run -d -p 9000:9000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:lts
+
+echo "# Dry"
+curl -sSf https://moncho.github.io/dry/dryup.sh | sh
+chmod 755 /usr/local/bin/dry
+
+echo "# Trivy"
+curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | \sh -s -- -b /usr/local/bin latest
+
+echo "Hadolint"
+
+HADOLING_VERSION=$(curl -sL "https://api.github.com/repos/hadolint/hadolint/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
+curl -fLo hadolint "https://github.com/hadolint/hadolint/releases/download/v${HADOLING_VERSION}/hadolint-Linux-x86_64"
+chmod +x hadolint
+mv -v hadolint /usr/local/bin/
